fd4699aaf46abbf7de25af568805542d4dc16c48
Ashish1331 <akumarmishr2@opentext.com>
2024-05-28 15:38:50 +0530

    Created and added

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPO_PATH="./"
+COMMIT_MESSAGE="Created and added"
+BRANCH_NAME="master" 
+
+cd $REPO_PATH
+
+if [ ! -d "$REPO_PATH" ]; then
+  echo "Directory $REPO_PATH does not exist."
+  exit 1
+fi
+git init
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git remote add origin https://github.com/Ashish1331/demo.git
+git push -u origin $BRANCH_NAME
+
+echo "Repository updated successfully."
+
